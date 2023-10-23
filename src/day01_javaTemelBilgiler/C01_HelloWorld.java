588e194cfeb668aedc1ffaee265bdcf94e0d9a4f
bulutluoz <abbulutluoz@gmail.com>
2023-10-23 16:30:44 +0200

    ilk gun

---
+++ src/day01_javaTemelBilgiler/C01_HelloWorld.java
@@ -0,0 +1,24 @@
+package day01_javaTemelBilgiler;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("Babam");
+
+        // tek satirlik aciklamalar icin kullanilir
+
+        /*
+            buraya
+            yazacagimiz
+            kodlar
+            java tarafindan
+            compile edilmez
+         */
+
+
+    }
+
+}
---
+++ src/day01_javaTemelBilgiler/C02_VariableKullanimi.java
@@ -0,0 +1,34 @@
+package day01_javaTemelBilgiler;
+
+public class C02_VariableKullanimi {
+
+    public static void main(String[] args) {
+
+        int sayi = 20 ;
+
+        System.out.println(sayi); // 20
+
+
+        sayi = 30;
+
+        System.out.println(sayi);
+
+        sayi = sayi + 10 ;
+
+        System.out.println(sayi);// 40
+
+        sayi = 2 * sayi + 5 ;
+
+        System.out.println(sayi); // 85
+
+
+        // sayi = 4.6;
+        // sayi = "Java";
+
+        boolean bl = true ;
+        char chr = 'G';
+        String str = "Java candir";
+        int sy = 45;
+        double dbl = 5.6 ;
+    }
+}
