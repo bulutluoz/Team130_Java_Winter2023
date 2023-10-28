501e449f309e5184a4edd5f933e95a7c7fb006df
bulutluoz <abbulutluoz@gmail.com>
2023-10-28 16:52:20 +0200

    6.gun sonu

---
+++ src/day06_ifElseStatements/C01_IfStatements.java
@@ -0,0 +1,47 @@
+package day06_ifElseStatements;
+
+import java.util.Scanner;
+
+public class C01_IfStatements {
+
+    public static void main(String[] args) {
+
+        /*
+            Bagimsiz if cumleleri
+            kodun geriye kalani ile ilgilenmez
+            SADECE if parantezindeki boolean sarta odaklanir
+            boolean sart ==> true ise if body'si calisir
+            boolean sart ==> false ise if body'si calismaz
+         */
+
+        // kullanicidan 2 tamsayi alin
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen iki tamsayi giriniz");
+
+        int sayi1 = scanner.nextInt();
+        int sayi2 = scanner.nextInt();
+
+        // 1.sayi, 2.sayidan buyukse aferin yazdirin
+
+        if ( sayi1 > sayi2 ){
+            System.out.println("Aferin");
+        }
+
+        // 1.sayi cift ise cift sayilari severim yazdirin
+
+        if (sayi1 % 2 == 0){
+            System.out.println("cift sayilari severim");
+        }
+
+        // 2.sayi 50'den buyukse buyuk sayilarla isim olmaz yazdirin
+
+        if (sayi2 > 50) {
+            System.out.println("buyuk sayilarla isim olmaz");
+        }
+        // toplamlari 100'den kucukse ufak sayilarla isim olmaz yazdirin
+
+        if (sayi1 + sayi2 < 100){
+            System.out.println("ufak sayilarla isim olmaz");
+        }
+    }
+}
