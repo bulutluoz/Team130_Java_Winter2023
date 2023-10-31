662314cefa4b8b94a89e3325368eda64db107eed
bulutluoz <abbulutluoz@gmail.com>
2023-10-31 20:14:05 +0100

    8.gun sonu

--- src/day08_nestedIfElseStatements/C04_NestedIfElseEmeklilik.java
+++ src/day08_nestedIfElseStatements/C04_NestedIfElseEmeklilik.java
@@ -6,7 +6,7 @@ public class C04_NestedIfElseEmeklilik {
 
     public static void main(String[] args) {
         //Soru 1- Kullanicidan cinsiyetini ve yasini alin,
-        //        Kadin, 60 yas ve uzeri , Erkek 65 yas ve uzeri emekli olabilir.
+        //        Kadin 60 yas ve uzeri , Erkek 65 yas ve uzeri emekli olabilir.
         //        Cinsiyet ve yasini dikkate alarak “Emekli olabilirsin”
         //        veya “Emekli olmak icin .. yil daha calisman gerekir” yazdirin.
 
@@ -20,6 +20,32 @@ public class C04_NestedIfElseEmeklilik {
         System.out.println("Lutfen yasinizi giriniz");
 
         int yas = scanner.nextInt();
+
+        // Ana degisken cinsiyet olsun
+
+        if (cinsiyet == 'E'){
+            // erkekler
+            if (yas>80 || yas <15){
+                System.out.println("Bu yaslar icin emeklilik sorgumuz calismiyor");
+            } else if (yas>=65) {
+                System.out.println("Erkek emekli olabilir");
+            }else{
+                System.out.println("Erkek emekli olmak icin " + (65-yas) + " sene daha calismalidir");
+            }
+
+        } else if (cinsiyet == 'K') {
+            // Kadinlar
+            if (yas>75 || yas <15){
+                System.out.println("Bu yaslar icin emeklilik sorgumuz calismiyor");
+            } else if (yas>=60) {
+                System.out.println("Kadin emekli olabilir");
+            }else{
+                System.out.println("Kadin emekli olmak icin " + (60-yas) + " sene daha calismalidir");
+            }
+
+        }else{
+            System.out.println("Cinsiyet icin E veya K secmelisiniz");
+        }
 
     }
 }
