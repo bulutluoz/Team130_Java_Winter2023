951384cbc589d4d81338809d85dc51fdc1ff8faf
bulutluoz <abbulutluoz@gmail.com>
2023-11-15 21:17:07 +0100

    19.gun sonu

--- src/day19_arrays/C04_KullanimAdediniBulma.java
+++ src/day19_arrays/C04_KullanimAdediniBulma.java
@@ -8,6 +8,40 @@ public class C04_KullanimAdediniBulma {
 
         String[] harfler = {"a","c","r","s","a","a","s","k","a"};
 
+        String arananHarf = "m";
 
+        int sayac = 0;
+
+        for (int i = 0; i < harfler.length ; i++) {
+
+            if (harfler[i].equals(arananHarf)){
+                sayac++;
+            }
+        }
+
+        if (sayac == 0){
+            System.out.println("Verilen " + arananHarf + " harfi array'de yok");
+        }else {
+            System.out.println("Verilen " + arananHarf + " harfi array'de " + sayac + " kere kullanilmis");
+        }
+
+    }
+
+    public static void elemanArama(String[] arr , String arananEleman){
+
+        int sayac = 0;
+
+        for (int i = 0; i < arr.length ; i++) {
+
+            if (arr[i].equals(arananEleman)){
+                sayac++;
+            }
+        }
+
+        if (sayac == 0){
+            System.out.println("Istenen eleman " + arananEleman + " harfi array'de yok");
+        }else {
+            System.out.println("Istenen eleman " + arananEleman + ", array'de " + sayac + " kere kullanilmis");
+        }
     }
 }
--- src/day19_arrays/Runner.java
+++ src/day19_arrays/Runner.java
@@ -9,6 +9,7 @@ public class Runner {
     public static void main(String[] args) {
 
         int[] sayilar = {3,-5,4,6,-2,9,1};
+        String[] isimler = {"ali","veli","saido","sahil","ali","saido","mehmet"};
 
 
         // sayilar array'indeki tum sayilari 3 artirip, kaydedin
@@ -20,5 +21,17 @@ public class Runner {
         // sayilar array'inin yeni halinde pozitif elementlerinin toplamini yazdirin
 
         System.out.println("pozitif elementlerin toplami : "+ C03_pozitifSayilariToplama.pozitifElementleriTopla(sayilar));
+
+
+        // isimler array'inde mehmet ismi kac kere kullanilmis, yazdirin
+        C04_KullanimAdediniBulma.elemanArama(isimler,"mehmet");
+        // Istenen eleman mehmet, array'de 1 kere kullanilmis
+
+
+        // isimler array'inde ali ismi kac kere kullanilmis, yazdirin
+        C04_KullanimAdediniBulma.elemanArama(isimler,"ali");
+        //Istenen eleman ali, array'de 2 kere kullanilmis
+
+
     }
 }
